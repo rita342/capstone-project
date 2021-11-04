ffc14d3251ead3db23ad346c7d4e760dc36bd30c
Rahel Yohannes Woticha <83081065+rita342@users.noreply.github.com>
2021-11-04 12:01:44 +0000

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,48 @@
 import {Button} from 'react-bootstrap'
-import { BiSearchAlt2 } from "react-icons/bi";
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
+import { useState } from 'react'
 
+function Header() {
 
-function Header(){
-    return(  
-        <div className="container" id="header">
+    const [searchInput, setSearchInput]= useState("");
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
 
-       </div> 
+    const handleSelect = (ranges) => {
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.endDate);
+    }
+    
+    const selectionRange = {
+        startDate:startDate,
+        endDate:endDate,
+        key:'selection',
+    }
+   
+
+    return( 
+      
+      <container>
+        <div className="container" id="header" style={{textAlign:'center',margin:'10px'}}>
+    <input  value ={searchInput} onChange ={(e) => setSearchInput(e.target.value)}type = "text" placeholder="  start your search"  />
+  
+   
+        </div>
+       {searchInput && (  
+       <div>
+<DateRangePicker
+ranges={[selectionRange]}
+onChange={handleSelect}
+/>
+           </div>
+        ) }
+        </container>
+       
+            
+        
+            
     );
 }
 export default Header
