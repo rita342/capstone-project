0719369ecbb3692ca6af44ebf777830717ce247c
Rahel Yohannes Woticha <83081065+rita342@users.noreply.github.com>
2021-11-05 14:42:31 +0000

    commit

--- src/components/MediumCard.jsx
+++ src/components/MediumCard.jsx
@@ -3,6 +3,7 @@ import imgpet from '../images/img-pet.jpg'
 import img1 from '../images/img1.jpg'
 import img2 from '../images/img2.jpg'
 import img3 from '../images/img3.jpg'
+import img4 from '../images/imgcrop.jpg'
 import {Col,Container,Row} from 'react-bootstrap'
 
 
@@ -15,29 +16,37 @@ import {Col,Container,Row} from 'react-bootstrap'
     <Col xs={12} md={6} lg={6} xl={3}>
     
               <img src={img1} layout="fill" style={{width:'270px',borderRadius:'10px'}}/>   
-              <h4>Outdoor getways</h4>  
+              <h5>Outdoor getways</h5>  
          
          </Col>
     <Col  xs={12} md={6} lg={6} xl={3}> 
     
           
           <img src={img2} layout="fill" style={{width:'270px',borderRadius:'10px'}}/>  
-          <h4>Unique Stays</h4>     
+          <h5>Unique Stays</h5>     
      
      </Col>
     <Col xs={12} md={6} lg={6} xl={3}> 
     
      <img src={img3} layout="fill" style={{width:'270px',borderRadius:'10px'}}/>  
-     <h4>Entire homes</h4>     
+     <h5>Entire homes</h5>     
 
 </Col>
     <Col xs={12} md={6} lg={6} xl={3}>
    
  <img src={imgpet} layout="fill" style={{width:'270px',borderRadius:'10px'}}/>  
- <h4>Pet allowed</h4>     
+ <h5>Pet allowed</h5>     
 
 </Col>
   </Row>
+  
+  <Row>
+    <Col xs={12} md={12} lg={12}><img src={img4} layout="fill" objectFit="cover"  style={{width:'100%',borderRadius:'10px',height:'400px',marginTop:'5rem',marginBottom:'5rem'}}/> 
+    <h2 style={{marginTop:'-40%'}}>dfff</h2>
+     </Col>
+    
+  </Row>
+
 </Container>
             
                
